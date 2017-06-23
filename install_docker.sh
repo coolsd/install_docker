290212a2613e73757d226b39fc0c0f9670136196
Sakda Chutchawan <sakda@nstda.or.th>
2017-06-23 12:16:15 +0700

    first commit

---
+++ install_docker.sh
@@ -0,0 +1,14 @@
+apt-get remove -y docker docker-compose
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+ $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-get install -y docker-ce
+docker -v
+
+curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
