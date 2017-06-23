c8b64ec46c3335084ccbe8e28f64990db51384e8
Sakda Chutchawan <sakda@nstda.or.th>
2017-06-23 13:44:56 +0700

    fix bug

--- install_docker.sh
+++ install_docker.sh
@@ -1,4 +1,5 @@
 apt-get remove -y docker docker-compose
+apt-get update
 apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
